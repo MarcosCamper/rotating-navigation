5c285398f36f402c9281894e671b0bbeeb2620a1
MarcosCamper <marcosmgfl@gmail.com>
2022-08-05 10:53:18 -0300

    adicionado comentarios ao arquivo js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const open = document.getElementById('open');
-const close = document.getElementById('close');
-const container = document.querySelector('.container');
+const open = document.getElementById('open'); //puxando do documento html o id 'open' e colocando como uma constante //
+const close = document.getElementById('close'); //puxando do documento html o id 'close' e colocando como uma constante //
+const container = document.querySelector('.container'); //colocando container como constante e usando queryselector pq só há um //
 
-open.addEventListener('click',() => container.classList.add('show-nav'));
+open.addEventListener('click',() => container.classList.add('show-nav')); //ao clicar no botao open, será adicionado ao container a class show-nav //
 
-close.addEventListener('click',() => container.classList.remove('show-nav'));
+close.addEventListener('click',() => container.classList.remove('show-nav')); //ao clicar no botao close, será removido show-nav do container //
